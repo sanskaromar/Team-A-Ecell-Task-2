b1258ba367a9fbb2648fc6a1bfc10f2356b13252
PANDEYS432 <98703881+PANDEYS432@users.noreply.github.com>
2023-01-03 19:32:14 +0530

    initialized startup page
    startups by MNNITians page is initialized

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/header/Header";
 import About from "./components/About/about";
 import Renaissance from "./components/Renaissance/Renaissance";
+import Startups from "./components/Startups/Startups";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Renaissance />
+      <Startups />
     </div>
   );
 }
